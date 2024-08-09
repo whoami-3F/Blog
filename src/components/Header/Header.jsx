4f3cf27f5ec82d3340b09fa92480de1a5f43911f
whoami-3F <saurav.stamang@gmail.com>
2024-08-09 22:12:41 +0545

    adding file and modifying compoment

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,49 @@
+import { Container, LogoutBtn, Logo } from "../index";
+import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function Header() {
+  const authStatus = useSelector((state) => state.authStatus);
+  const navigate = useNavigate();
+
+  const navItems = [
+    { name: "Home", slug: "/", active: true },
+    { name: "Login", slug: "/login", active: !authStatus },
+    { name: "Signup", slug: "/signup", active: !authStatus },
+    { name: "All Posts", slug: "/all-posts", active: authStatus },
+    { name: "Add Posts", slug: "/add-posts", active: authStatus },
+  ];
   return (
-    <div>
-      <h1>Header</h1>
-    </div>
+    <header>
+      <Container>
+        <nav className="flex">
+          <div className="mr-4">
+            <Link to="/">
+              <Logo width="70px" />
+            </Link>
+          </div>
+          <ul className="flex ml-auto">
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className="inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+                  >
+                    {item.name}
+                  </button>
+                </li>
+              ) : null,
+            )}
+            {/* if the authStatus is true */}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Logo from "./Logo";
+import Container from "./container/Container";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
 
-export { Header, Footer };
+export { Header, Footer, Logo, Container, LogoutBtn, Button };
