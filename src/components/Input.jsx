f19d514f5df0b0945c20e06f5835f948b502c2af
whoami-3F <saurav.stamang@gmail.com>
2024-08-11 15:53:34 +0545

    stop tracking some file

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,5 @@
-import { useId } from "react";
+import { useId, forwardRef } from "react";
+
 const Input = forwardRef(function Input(
   { label, type = "text", className = "", ...props },
   ref,
@@ -7,14 +8,14 @@ const Input = forwardRef(function Input(
   return (
     <div className="w-full">
       {label && (
-        <label className="inline-block mb-1 pl-1" htmlFor="{id}">
+        <label className="inline-block mb-1 pl-1" htmlFor={id}>
           {label}
         </label>
       )}
       <input
         type={type}
-        className={`px-3 py-2 rounded-lg bg-white text-black outline-none
-          focus:bg-gray-50 duration-200 border border-gray-200 w-full`}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50
+                    duration-200 border border-gray-200 w-full ${className}`}
         ref={ref}
         {...props}
         id={id}
@@ -22,4 +23,5 @@ const Input = forwardRef(function Input(
     </div>
   );
 });
+
 export default Input;
